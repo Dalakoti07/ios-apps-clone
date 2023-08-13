ba81617499ede0b7c2702cdfeffd600f65ae018a
dalakoti07 <saurabhdalakoti07@gmail.com>
2023-08-13 22:01:44 +0530

    Added: pokemon project

---
+++ pokemonApp/pokemonApp/pokemonAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  pokemonAppApp.swift
+//  pokemonApp
+//
+//  Created by Saurabh Dalakoti on 13/08/23.
+//
+
+import SwiftUI
+
+@main
+struct pokemonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
